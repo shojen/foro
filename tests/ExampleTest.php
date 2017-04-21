15776dfe6952ef6aa74849ff18cf20b67713195e
shojen <aarpwebmaster@gmail.com>
2017-04-21 19:14:23 +0200

    primer commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    use DatabaseTransactions;
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $user=factory(App\User::class)->create([
+                'name' => 'Angel Rosso'
+            ]);
+
+        $this->actingAs($user,'api')
+            ->visit('api/user')
+            ->see('Angel Rosso');
+    }
+}
