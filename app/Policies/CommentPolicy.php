d481aa296249c99116b410c834c380692c32a1c3
shojen <aarpwebmaster@gmail.com>
2017-08-06 19:19:54 +0000

    Marcar un comentario como respuesta de un post usando TDD en Laravel 5.3

---
+++ app/Policies/CommentPolicy.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Policies;
+
+use App\Comment;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CommentPolicy
+{
+    use HandlesAuthorization;
+
+
+
+    public function accept(User $user,Comment $comment)
+    {
+        return $user->owns($comment->post);
+    }
+}
